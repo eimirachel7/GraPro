7f530e626c1ccb802712dac5c9383cec3c6e738f
hungtranthe55@gmail.com <hungtranthe55@gmail.com>
2023-06-26 17:19:05 +0700

    contrast 50, saturation 80

--- def_matrixmove.py
+++ def_matrixmove.py
@@ -8,9 +8,9 @@ mat = np.matrix
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(3,640)
 cap.set(4,480)
-cap.set(10,50)
+cap.set(10,0)
 cap.set(11,50)
-cap.set(12,100)
+cap.set(12,80)
 
 while True:
     ret, frame = cap.read()
