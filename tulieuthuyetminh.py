976b81969a29e248091bdc2e8f35447b563ee258
hungtranthe55@gmail.com <hungtranthe55@gmail.com>
2023-07-03 23:19:13 +0700

    sua truoc khi di choi

--- tulieuthuyetminh.py
+++ tulieuthuyetminh.py
@@ -6,12 +6,12 @@ import numpy as np
 fig = plt.figure(figsize=(16, 9)) # Tạo vùng vẽ tỷ lệ 16:9
 ax1, ax2 = fig.subplots(1, 2) # Tạo 6 vùng vẽ con
 
-dub = cv.imread('hustlogo.png')
-ax1.imshow(dub, cmap='gray')
+anh1 = cv.imread('hustlogo.png')
+ax1.imshow(anh1, cmap='gray')
 ax1.set_title("Ảnh máy đọc BGR")
 
-dub_real = cv.cvtColor(dub, cv.COLOR_BGR2RGB)
-ax2.imshow(dub_real, cmap='gray')
+anh2 = cv.cvtColor(anh1, cv.COLOR_BGR2RGB)
+ax2.imshow(anh2, cmap='gray')
 ax2.set_title("Ảnh thật RGB")
 
 plt.show()
