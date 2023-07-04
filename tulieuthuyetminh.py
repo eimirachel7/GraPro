c9cf2936ed43642fd3b850b4a6512726cdc3aae6
hungtranthe55@gmail.com <hungtranthe55@gmail.com>
2023-07-04 17:16:38 +0700

    them file tai lieu

--- tulieuthuyetminh.py
+++ tulieuthuyetminh.py
@@ -1,17 +1,24 @@
-import cv2 as cv
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-fig = plt.figure(figsize=(16, 9)) # Tạo vùng vẽ tỷ lệ 16:9
-ax1, ax2 = fig.subplots(1, 2) # Tạo 6 vùng vẽ con
+# fig = plt.figure(figsize=(16, 9)) # Tạo vùng vẽ tỷ lệ 16:9
+# ax1, ax2 = fig.subplots(1, 2) # Tạo 6 vùng vẽ con
 
-anh1 = cv.imread('hustlogo.png')
-ax1.imshow(anh1, cmap='gray')
-ax1.set_title("Ảnh máy đọc BGR")
+# anh1 = cv2.imread('hustlogo.png')
+# ax1.imshow(anh1, cmap='gray')
+# ax1.set_title("Ảnh máy đọc BGR")
 
-anh2 = cv.cvtColor(anh1, cv.COLOR_BGR2RGB)
-ax2.imshow(anh2, cmap='gray')
-ax2.set_title("Ảnh thật RGB")
+# anh2 = cv2.cvtColor(anh1, cv.COLOR_BGR2RGB)
+# ax2.imshow(anh2, cmap='gray')
+# ax2.set_title("Ảnh thật RGB")
 
-plt.show()
+# plt.show()
+
+img = cv2.imread("shapes.jpg", cv2.IMREAD_COLOR)
+
+
+cv2.imshow("shape", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
